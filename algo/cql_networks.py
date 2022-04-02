9b909809c7cf0ab728a0145039bb5a389d86b2e1
weiaiF <72204912+weiaiF@users.noreply.github.com>
2022-04-02 15:04:32 +0800

    Add files via upload

---
+++ algo/cql_networks.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Normal
+import numpy as np
+import torch.nn.functional as F
+
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, hidden_size=256, init_w=3e-3, log_std_min=-20, log_std_max=2):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+        
+        self.fc1 = nn.Linear(state_size, hidden_size)
+        self.fc1 = nn.DataParallel(self.fc1, device_ids = [0,1,2,3])
+
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.DataParallel(self.fc2, device_ids = [0,1,2,3])
+
+        self.mu = nn.Linear(hidden_size, action_size)
+        self.mu = nn.DataParallel(self.mu, device_ids = [0,1,2,3])
+
+        self.log_std_linear = nn.Linear(hidden_size, action_size)
+        self.log_std_linear = nn.DataParallel(self.log_std_linear, device_ids = [0,1,2,3])
+
+    def forward(self, state):
+
+        # print(state.shape)
+        # print(self.fc1)
+        x = F.relu(self.fc1.module(state))
+        x = F.relu(self.fc2.module(x))
+        mu = self.mu.module(x)
+
+        log_std = self.log_std_linear.module(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        return mu, log_std
+    
+    def evaluate(self, state, epsilon=1e-6):
+        mu, log_std = self.forward(state)
+        std = log_std.exp()
+        dist = Normal(mu, std)
+        e = dist.rsample().to(state.device)
+        action = torch.tanh(e)
+        log_prob = (dist.log_prob(e) - torch.log(1 - action.pow(2) + epsilon)).sum(1, keepdim=True)
+
+        return action, log_prob
+        
+    
+    def select_action(self, state):
+        """
+        returns the action based on a squashed gaussian policy. That means the samples are obtained according to:
+        a(s,e)= tanh(mu(s)+sigma(s)+e)
+        """
+        mu, log_std = self.forward(state)
+        std = log_std.exp()
+        dist = Normal(mu, std)
+        e = dist.rsample().to(state.device)
+        action = torch.tanh(e)
+        return action.detach().cpu()
+    
+    def get_det_action(self, state):
+        mu, log_std = self.forward(state)
+        return torch.tanh(mu).detach().cpu()
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, hidden_size=256, seed=1):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            hidden_size (int): Number of nodes in the network layers
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size+action_size, hidden_size)
+        # print(self.fc1)
+        # print(type(self.fc1))
+        self.fc1 = nn.DataParallel(self.fc1, device_ids = [0,1,2,3])
+        # print(self.fc1.module)
+        # print(type(self.fc1.module))
+
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.DataParallel(self.fc2, device_ids = [0,1,2,3])
+
+        self.fc3 = nn.Linear(hidden_size, 1)
+        self.fc3 = nn.DataParallel(self.fc3, device_ids = [0,1,2,3])
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.module.weight.data.uniform_(*hidden_init(self.fc1.module))
+        self.fc2.module.weight.data.uniform_(*hidden_init(self.fc2.module))
+        self.fc3.module.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        x = torch.cat((state, action), dim=-1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
